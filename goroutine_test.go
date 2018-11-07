0af21d07cfec84daf0484f861052672df17177fa
kevin <18717825632@163.com>
2018-11-07 10:33:38 +0800

    添加了goroutine测试过程描述

--- goroutine_test.go
+++ goroutine_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+//测试过程为，打开terminal top和编辑器，让内存保持稳定进行测试
 //goroutine测试，用top查看mem内存后得出20万个goroutine占用500MB内存空间，占用512000KB，每个goroutine = 512000/200000 =  2.56KB
 //相当于一个goroutine内存开销为2.5KB
 func Test_goroutine(t *testing.T) {
